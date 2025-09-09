6d2e8bbd9bef1782bb7dc510f7b8e9e2860c292f
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-09 17:53:18 +0200

    excersise 2-10

---
+++ chapter_two/tiy_2-10.py
@@ -0,0 +1,9 @@
+FAVORITE_NUM = 6
+num = FAVORITE_NUM
+
+question = "Me: Hey Python, did you know?"
+answer = "Python: What?"
+message = "My favorite number is: "
+
+
+print(f"{question}\n{answer}\n{message}{num}!")
