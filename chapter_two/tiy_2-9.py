3ed9790ccd9d38dc4ec9df9ee815ec1b2bba623e
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-09 17:41:31 +0200

    excercise 2-9

---
+++ chapter_two/tiy_2-9.py
@@ -0,0 +1,13 @@
+print("8 reasons why:")
+add = 4+4
+print(f"4 + 4 = {add}")
+sub = 12-4
+print(f"12 - 4 = {sub}")
+mul = 2*4
+print(f"2 x 4 = {mul}")
+exp = 2**3
+print(f"2 ** 3 = {exp}")
+div = 16/2
+print(f"16 : 2 = {div}")
+print(f"Oopsie... that was 5...")
+print(f"Oh well xD")
