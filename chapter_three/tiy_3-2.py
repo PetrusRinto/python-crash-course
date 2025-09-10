f7fa1c8ca764a94a15e273a200df2e5061d5d422
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-10 17:05:44 +0200

    Tried a for loop just for fun, and it worked :D

---
+++ chapter_three/tiy_3-2.py
@@ -0,0 +1,9 @@
+# Patrick 10.09.2025
+# This program stores my friends in a list
+# And I shall write them a message each
+
+names = ['Rebekka', 'Natalie', 'Bjørn-André', 'Morten', 'Dina', 'Kristian', 'Tim']
+message = "is my friend!"
+
+for name in names:
+    print(f"{name} {message}")
