90a8c93a8c46cf8ff6a3c610f105757c5a4893b9
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-10 16:46:50 +0200

    friend?

---
+++ chapter_three/tiy_3-1.py
@@ -0,0 +1,12 @@
+# Patrick 10.09.2025
+# This program stores my friends in a list, and I shall list them one-by-one
+
+names = ['Rebekka', 'Natalie', 'Bjørn-André', 'Morten', 'Dina', 'Kristian', 'Tim']
+
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
+print(names[6])
