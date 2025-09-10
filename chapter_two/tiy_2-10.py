e8591ed31afcc9c748dcd5e296c70e072d9ca86f
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-10 06:09:12 +0200

    added comments

--- chapter_two/tiy_2-10.py
+++ chapter_two/tiy_2-10.py
@@ -1,9 +1,12 @@
-FAVORITE_NUM = 6
-num = FAVORITE_NUM
+# Patrick 10.09.2025
+# A program that output my facorite number in an entertaining way 
+
+FAVORITE_NUM = 6 # constant
+num = FAVORITE_NUM # simplified
 
 question = "Me: Hey Python, did you know?"
 answer = "Python: What?"
 message = "My favorite number is: "
 
 
-print(f"{question}\n{answer}\n{message}{num}!")
+print(f"{question}\n{answer}\n{message}{num}!") # f-string to output multiple variables.
