1e219a66f4d6f0c5ad48c496af944cf482e51c08
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-11 17:11:17 +0200

    the greatest list

---
+++ chapter_three/tiy_3-4.py
@@ -0,0 +1,7 @@
+# Patrick 11.09.2025
+# The great 'guest list'
+
+guests = ['jesus', 'ghandi', 'hawkins', 'stark', 'rudeus', 'kazuto', 'iroh', 'katara']
+
+for guest in guests:
+    print(f"Dear {guest.title()}, I humbly invite you to the greatest dinner this world has ever seen. I would sincerily appreciate your attendence.")
