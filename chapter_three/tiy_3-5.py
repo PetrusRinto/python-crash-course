97ccf887c2374cf528a90453c00a867f1c94e260
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-12 07:10:23 +0200

    Removed Ghandi and added Gwen

--- chapter_three/tiy_3-5.py
+++ chapter_three/tiy_3-5.py
@@ -2,7 +2,28 @@
 # The great 'guest list'
 # With changes
 
-guests = ['jesus', 'ghandi', 'hawkins', 'stark', 'rudeus', 'kazuto', 'iroh', 'katara']
+guests = ['jesus', 'ghandi', 'hawkins', 'stark', 'rudeus', 'kazuto', 'iroh', 'katara'] # Same list
+
+# Adding a newline for aesthetics
+print('\n')
 
 for guest in guests:
-    print(f"Dear {guest.title()}, I humbly invite you to the greatest dinner this world has ever seen. I would sincerily appreciate your attendence.")
+    print(f"Dear {guest.title()}, I invite you to a humble dinner party. Well met!") # Looped because I am lazy
+
+# Oh no, Ghandi seems to be busy
+too_busy = 'ghandi'
+guests.remove(too_busy) # Removing Ghandi from the list
+
+print(f'\nUnfortunately {too_busy.title()} is busy and can not attend.') # Sad message :(
+
+# Wait, I can invite someone else to take Ghandi's place!
+new_guest = 'gwen' # No other than Gwen Stacy herself
+guests.append(new_guest) # Adding Gwen to the list
+
+print('\n') # Newline
+
+# Reusing the loop from before
+for guest in guests:
+    print(f"Dear {guest.title()}, I invite you to a humble dinner party. Well met!")
+    # Output: The same list, but Ghandi gone and Gwen present
+print('\n') # Newline
