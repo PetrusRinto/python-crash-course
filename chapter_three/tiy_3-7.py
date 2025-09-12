97e7d0c5aafc7269c44743de551d5f892d3b8770
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-12 17:13:32 +0200

    No more big table...

---
+++ chapter_three/tiy_3-7.py
@@ -0,0 +1,42 @@
+# Patrick 12.09.2025
+# The great 'guest list'
+# With more changes, but less people
+
+guests = ['jesus', 'ghandi', 'hawkins', 'stark', 'rudeus', 'kazuto', 'iroh', 'katara'] # Same list
+
+# Adding a newline for aesthetics
+print('\n')
+
+# Oh no, Ghandi seems to be busy
+too_busy = 'ghandi'
+guests.remove(too_busy) # Removing Ghandi from the list
+
+# Wait, I can invite someone else to take Ghandi's place!
+new_guest = 'gwen' # No other than Gwen Stacy herself
+guests.insert(1, new_guest) # Adding Gwen to the list
+
+# Reusing the loop from before
+for guest in guests:
+    print(f"Dear {guest.title()}, I invite you to a humble dinner party. Well met!")
+    # Output: The same list, but Ghandi gone and Gwen present
+print('\n') # Newline
+
+# Informing of more space
+print('Good news! I found a bigger table.')
+
+# Defineing extra guests for the bigger table
+extra_guest1 = 'zuko'
+extra_guest2 = 'aang'
+extra_guest3 = 'asami'
+
+guests.append(extra_guest1)
+guests.append(extra_guest2)
+guests.append(extra_guest3)
+
+print('\n') # Newline
+
+# Reusing the loop from before
+for guest in guests:
+    print(f"Dear {guest.title()}, I invite you to a humble dinner party. Well met!")
+    # Output: The same list, but Ghandi gone and Gwen present
+print('\n') # Newline
