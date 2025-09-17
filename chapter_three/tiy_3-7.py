af63d0ffc2ca4c0698e8d168f5cdcfe0f25abc13
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-17 16:49:55 +0200

    Had to compromise and reducing the guest amount

--- chapter_three/tiy_3-7.py
+++ chapter_three/tiy_3-7.py
@@ -40,3 +40,38 @@ for guest in guests:
     print(f"Dear {guest.title()}, I invite you to a humble dinner party. Well met!")
     # Output: The same list, but Ghandi gone and Gwen present
 print('\n') # Newline
+
+print(f"I can only invite two people for dinner. This is a result of the table not being able to arrive in time.")
+print('\n') # Newline
+
+# Popping guests who are uninvited
+popped_one = guests.pop(0)
+
+popped_two = guests.pop(1)
+
+popped_three = guests.pop(2)
+
+popped_four = guests.pop(3)
+
+popped_five = guests.pop(4)
+
+popped_six = guests.pop(5)
+
+popped_seven = guests.pop(-4)
+
+popped_eight = guests.pop(-3)
+
+popped_nine = guests.pop(-2)
+
+# Storing the uninvited guests to a variable
+popped_guests = [popped_one, popped_two, popped_three, popped_four, popped_five, popped_six, popped_seven, popped_eight, popped_nine]
+
+# Looping the list to print an apology
+for popped in popped_guests:
+    print(f'I am so sorry {popped}, but we do not have the capacity and cannot invite you.')
+print('\n') # Newline
+
+# Letting the remaining guests know they are invited
+for invited in guests:
+    print(f'Good news {invited}! you are still invited. Congrats!')
+print('\n') # Newline
