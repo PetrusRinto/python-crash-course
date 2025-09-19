3773346fc4d3904d6d49cba53fdaee65316df69f
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-19 17:37:07 +0200

    Changed titles for better naming practice

---
+++ chapter_three/tiy_three_nine.py
@@ -0,0 +1,7 @@
+# Patrick 19.09.2025
+# "Dinner Guests: Working with one of the programs from Excercises 3-4 through 3-7,
+# I have to use len() to print a message indicating the number of people I'm inviting to dinner.
+
+from tiy_three_seven import guests, guest
+
+print(len(guests))
