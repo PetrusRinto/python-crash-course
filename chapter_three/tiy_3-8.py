0a5d80bc065115a9906b362edf9c8e586e8fe83c
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-19 10:09:41 +0200

    Added comments and cleaner output

--- chapter_three/tiy_3-8.py
+++ chapter_three/tiy_3-8.py
@@ -5,17 +5,22 @@
 places = ['hokkaido', 'zanzibar', 'beijing', 'hong kong', 'nordkapp']
 
 # printing in original order
+print(f'The list displayed in original order:')
 print(places)
 
 # temporarily sorting the list in alphabetical order
-print(sorted(places))
+print(f'\nThe list displayed in alphabetical order:')
+print(f'{sorted(places)}')
 
 # verifying that the change was temporary
+print(f'\nVerify the temporary change:')
 print(places)
 
 # temporarily sorting in reverse order
 reverse_sorted = sorted(places, reverse=True) # 18.09.2025
+print(f'\nThe list displayed in reverse-alphabetical order:') # 19.09.2025
 print(reverse_sorted)
 
 # verifying the list change
+print(f'\nVerify the temporary change:')
 print(places)
