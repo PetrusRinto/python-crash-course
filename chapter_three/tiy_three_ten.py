9f9a53b6377058c9d773b9ff3009456b9e8761ef
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-22 16:40:50 +0200

    reverse and reverse again

--- chapter_three/tiy_three_ten.py
+++ chapter_three/tiy_three_ten.py
@@ -4,9 +4,21 @@
 # A regular list of three countries (it will increase).
 countries = ['thailand', 'zanzibar', 'france']
 
-# a message that pints the list:
+# a message that prints the list:
 print(f'Original: {countries}')
 
 print(f'\nSorted: {sorted(countries)}') # Alphabetical order, but not permanent
 
+print(f'\nOriginal: {countries}')
+
+print(f'\nReverse alphabetically:{sorted(countries, reverse=True)}')
+
+print(f'\nOriginal: {countries}')
+
+# Using variables to sort diferentally and permanentally.
+countries.reverse()
+print(f'\nReverse originally: {countries}')
+
+# Reverse again outputs the original format
+countries.reverse()
 print(f'\nOriginal: {countries}')
