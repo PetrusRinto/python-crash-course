6adfc8a17b64294ea1b34fd36affed6aaf9fccc2
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-23 08:03:42 +0200

    fine tuned the code with more comments and corrections

--- chapter_three/tiy_three_ten.py
+++ chapter_three/tiy_three_ten.py
@@ -5,20 +5,33 @@
 countries = ['thailand', 'zanzibar', 'france']
 
 # a message that prints the list:
+print('This is the original list.')
 print(f'Original: {countries}')
 
-print(f'\nSorted: {sorted(countries)}') # Alphabetical order, but not permanent
+# Alphabetical order, but not permanent
+print(f'\nSorted in alphabetical order, but not permanent.')
+print(f'Sorted: {sorted(countries)}')
 
-print(f'\nOriginal: {countries}')
+# Proving the change was temporary
+print(f'\nThe original again to prove the temporary change.')
+print(f'Original: {countries}')
 
-print(f'\nReverse alphabetically:{sorted(countries, reverse=True)}')
+# Sorting in alphabetical, but reversed
+print(f'\nSorted in reverse-alphabetical order.')
+print(f'Reversed alphabetically:{sorted(countries, reverse=True)}')
 
-print(f'\nOriginal: {countries}')
+# Proving the temporary change by outputting the original
+print(f'\nProving the temporary change.')
+print(f'Original: {countries}')
 
-# Using variables to sort diferentally and permanentally.
+# Using variables to sort diferentally and permanentally
+print(f'\nCreating a variable to permanentally reverse the order.')
 countries.reverse()
-print(f'\nReverse originally: {countries}')
+print(f'Reversed original: {countries}')
 
 # Reverse again outputs the original format
+print(f'\nTo revert back to the original we have to revere it again.')
 countries.reverse()
-print(f'\nOriginal: {countries}')
+print(f'Original: {countries}')
+
+# Patrick 23.09.2025
