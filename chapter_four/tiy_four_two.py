db920ff91e1f61ca803a999886728748cb720a16
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-25 16:45:37 +0200

    looping animals

---
+++ chapter_four/tiy_four_two.py
@@ -0,0 +1,9 @@
+# Patrick 25.09.2025
+# 4-2, Animals: Three animals stored and printed in a for loop
+
+# List of catlike animals
+animals = ['cat', 'lion', 'tiger']
+
+# For loop to list each animal
+for animal in animals:
+    print(f'{animal}')
