90aba1768c6052e5777858806e19c2aac07c36a9
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-25 12:06:52 +0200

    Pizza loop

---
+++ chapter_four/tiy_four_one.py
@@ -0,0 +1,9 @@
+# Patrick 25.09.2025
+# 4-1 Pizzas: Three kinds of pizza, sored in a list loop a sentence
+
+# Pizza list
+pizzas = ['campino', 'napolitan', 'homemade']
+
+# Looping the pizzas
+for pizza in pizzas:
+    print(f'{pizza.title()}')
