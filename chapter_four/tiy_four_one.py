905b2692dc2dc0e577685a033c61037031799a74
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-25 12:24:54 +0200

    I really like pizza...

--- chapter_four/tiy_four_one.py
+++ chapter_four/tiy_four_one.py
@@ -11,4 +11,5 @@ pizzas = ['campino', 'napolitan', 'homemade']
 # Looping the pizzas in sentences
 for pizza in pizzas:
     print(f'I like {pizza} pizza!')
-print(f'Pizza is the perfect invention, you can make however you like. My way is the best of course.') # Added a line to express my passion of pizza.
+print(f'Pizza is the perfect invention, you can make it however you like. My way is the best of course.') # Added a line to express my passion of pizza.
+print(f'I could probably eat pizza every day!') # I really love pizza, in different words.
