6961265a836601d02bdf5d18a760ad02293803ef
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-26 06:48:23 +0200

    Ranges are interesting

--- chapter_four/test.py
+++ chapter_four/test.py
@@ -1,3 +1,6 @@
 # Patrick 26.09.2025
-for value in range(0, 5): # This is powerful
+for value in range(0, 6): # This is powerful
     print(value)
+print(f"\nThe 'range()' function in python ends on the second number '{value + 1}' in this case.")
+print(f"This means python will stop the range when the *next* number is the last value defined in the program.")
+print(f"Since the last number in our range() is '{value + 1}', it ends at '{value}'")
