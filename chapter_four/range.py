7282aa5bfbfada1345989b40523a73140192ac80
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-28 13:58:37 +0200

    The commit of humble-beginnings and hopefully the beginning of an end to my overcomplication-curse

---
+++ chapter_four/range.py
@@ -0,0 +1,5 @@
+# Patrick 28.09.2025
+# Testing a possible mistake at my end (overcomplication curse)
+
+for num in range(1, 21):
+	print(num)
---
+++ chapter_four/tiy_four_three.py
@@ -0,0 +1,17 @@
+# Patrick 28.09.2025
+# 4-3 Counting to twenty
+
+# A for loop that append the numbers 1 to 20 in the numbers' list
+numbers = []
+for num in range(1, 21):
+    numbers.append(num)
+# A variable associated with the appended numbers-list for the counting
+counting = numbers
+# A for loop that counts the numbers' list inclusively
+for count in counting:
+    print(count)
+# Upon this commit I realized this overcomplication at my end.
+# I got carried away with my new obtained knowledge from the course
+# and ended up with more line of code than necessary.
+# I decided to keep it though. That is because when I am done with this book,
+# I want it to be a reminder.
