2bded3abda51e7820719b1de82b63a81adc549a1
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-28 12:50:52 +0200

    List comprehensions is effective, but less readable to beginners

---
+++ chapter_four/squares.py
@@ -0,0 +1,8 @@
+# Patrick 28.09.2025
+# List Comprehensions
+# Combining the "for loop" and the creation of new elements into one line of code
+# and appends them.
+
+# Example of "squares" in a list comprehension
+squares = [ value**2 for value in range(1, 11)]
+print(squares)
