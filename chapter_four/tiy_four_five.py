707e462c9c42604b05b96e05fc39b3363017bf60
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-28 14:45:57 +0200

    Used sum() to test Python's quick math

--- chapter_four/tiy_four_five.py
+++ chapter_four/tiy_four_five.py
@@ -7,3 +7,6 @@
 # creating a list from one to one million
 one_million = [ million for million in range(1, 1000001) ]
 print(f'The list starts at: {min(one_million)}\nAnd ends at: {max(one_million)}') # Verify the list start at one and ends in one million
+
+# Using sum() function to see how quickly Python can handle adding one million numbers
+print(f'\nThe list added together: {sum(one_million)}')
