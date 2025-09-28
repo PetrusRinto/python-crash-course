2721f6706262e122eac6b571da6f80a320e4071f
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-28 14:41:14 +0200

    Created the list and verified with max and min

---
+++ chapter_four/tiy_four_five.py
@@ -0,0 +1,9 @@
+# Patrick 28.09.2025
+# 4-5 Summing a million
+# Making a list from one to a million, and using min() and max()
+# to start at one and end in one million.
+# Also, need to use sum() function to test pythons' quickness.
+
+# creating a list from one to one million
+one_million = [ million for million in range(1, 1000001) ]
+print(f'The list starts at: {min(one_million)}\nAnd ends at: {max(one_million)}') # Verify the list start at one and ends in one million
