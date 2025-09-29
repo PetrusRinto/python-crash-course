1824eb99c9e80811a6d71d65280984242ed5db97
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-29 12:10:03 +0200

    No comprehension in the comprehension assignment

---
+++ chapter_four/tiy_four_nine.py
@@ -0,0 +1,9 @@
+# Patrick 29.09.2025
+# 4-9 Cube Comprehension(X)
+# Since I used the cube comprehension in the last assignment to create the list
+# I will create a list of cubes without comprehension in this one.
+
+cubes = []
+for cube in range(1, 11):
+    cubes.append(cube**3)
+print(cubes)
