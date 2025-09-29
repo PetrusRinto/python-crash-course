24cbf674f26b535f4e952ed3010c3f69f508485a
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-29 11:49:32 +0200

    Odd numbers

---
+++ chapter_four/tiy_four_six.py
@@ -0,0 +1,9 @@
+# Patrick 28.09.2025
+# 4-6 Odd Numbers
+# A list that prints odd numbers in a for loop with range()
+
+# A odd list from 1 to 20
+odd_numbers = list(range(1, 20, 2)) # 29.09.2025
+# For loop to print each number
+for odd in odd_numbers:
+    print(odd)
