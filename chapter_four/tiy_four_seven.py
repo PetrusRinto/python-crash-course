d39966b4e6ec369fa1f1b9742a061f7d540f8cf5
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-29 11:53:56 +0200

    Multiplication table of three with a for loop

---
+++ chapter_four/tiy_four_seven.py
@@ -0,0 +1,9 @@
+# Patrick 29.09.2025
+# 4-7 Threes
+# A program that print the multiples of 3, from 3 to 30.
+
+threes = list(range(3, 31, 3))
+
+# For loop to print each multiplied number
+for three in threes:
+    print(three)
