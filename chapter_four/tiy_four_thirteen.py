ec56f424eef20b4f016a2c762bb0f6c2ed2a013a
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-30 11:50:43 +0200

    The menu is created as a tuple

---
+++ chapter_four/tiy_four_thirteen.py
@@ -0,0 +1,9 @@
+# Patrick 30.09.2025
+# 4-13 Buffet.
+# Storing menu in a tuple with five basic foods
+
+# Storing the menu as a tuple
+menu = ('soup', 'meatballs', 'spaghetti', 'beef', 'salad')
+# For loop to print each food.
+for food in menu:
+    print(food.title())
