b0a44d886f75f77d4bcbfbd8d9c630240a0cafeb
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-30 07:32:06 +0200

    For loops added from existing list in Eric Mathes's Python Crash Course book

---
+++ chapter_four/tiy_four_twelve.py
@@ -0,0 +1,19 @@
+# Patrick 30.09.2025
+# 4-12 More loops
+# Writing two for loops to print any foods.py program (from the python crash course book)
+
+# First chosen version of foods.py
+my_foods = ['pizza', 'falafel', 'carrot cake']
+friend_foods = my_foods[:]
+
+my_foods.append('cannoli')
+friend_foods.append('ice cream')
+
+print("My favorite foods are:")
+# Assignment 4-12
+for my_food in my_foods:
+    print(my_food.title())
+
+print("\nMy friend's favorite foods are:")
+for friend_food in friend_foods:
+    print(friend_food.title())
