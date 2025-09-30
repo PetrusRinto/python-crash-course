a2bbcb5c758fefdb8ffdea64f053e0496793f18d
Patrick Lintho <pettersenlintho@gmail.com>
2025-09-30 06:50:35 +0200

    Slice and dice 4-10

---
+++ chapter_four/tiy_four_ten.py
@@ -0,0 +1,17 @@
+# Patrick 29.09.2025
+# 4-10 Slices
+# Using one of my earlier programs to slice items
+
+# program from 4-9
+cubes = []
+for cube in range(1, 11):
+    cubes.append(cube**3)
+# 4-10 assignment
+print(f'All of the items in the list:')
+print(f'{cubes}\n')
+print(f'The first three items in the list are:')
+print(f'{cubes[:3]}\n')
+print(f'The three items from the middle of the list are:')
+print(f'{cubes[3:6]}\n')
+print(f'The last three items in the list are:')
+print(f'{cubes[-3:]}\n')
