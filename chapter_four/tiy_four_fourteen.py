02e81bd250dd0e027ed3021661f941b0d3475c80
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-01 12:08:51 +0200

    I say to myself that my future self will thank med for these notes

--- chapter_four/tiy_four_fourteen.py
+++ chapter_four/tiy_four_fourteen.py
@@ -3,3 +3,32 @@
 # PEP 8: «https://python.org/dev/peps/pep-0008»
 
 # Did not start reading until now - 01.10.2025
+
+# Introduction - Reflection
+# The PEP 8 document gives coding conventions for Python code.
+# Setting a convention for the Python language.
+# Convention basically means a standard for how something is or should be done.
+
+# Guido van Rossum and Barry Warsaw are the authors.
+# -Note-To-Self I want to know more about them as I read through this.
+# I find the author's name 'Guido' both ironic and hilarious - Respectfully
+# This is because it reminds me of the word 'guide' which PEP 8 is.
+
+# The introduction gives a brief background and inspirations for PEP 8
+# It was adapted from Guido's 'Pythong Style Guide essay', and Barry's guide.
+# Today's standard will probably be different over time and become outdated.
+# This is because the guide evolves as additional conventions comes to light.
+
+# A pattern I'd like to call 'Pyramid of Importance' in form of Consistency:
+#     Top (Important, but less)
+#         - Style guide
+#         - Project
+#         - Module or Function
+#     Buttom (Most important)
+
+# When breaking the rules are allowed:
+#     1. Make the code less readable, even for a PEP practitioner.
+#     2. To keep consitency within a project. (ref the above Pyramid)
+#     3. If the code in question only predates the introduction of the guideline.
+#     4. Rollback compatibility.
+
