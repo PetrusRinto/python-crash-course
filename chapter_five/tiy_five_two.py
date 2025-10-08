650657c328bd1cdb7b276fa222d759cec8e0f77b
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-08 17:32:59 +0200

    Conditions in a list or not in a list

--- chapter_five/tiy_five_two.py
+++ chapter_five/tiy_five_two.py
@@ -1,7 +1,7 @@
 # Patrick 08.10.2025.
 # More conditional tests.
 
-# Testing for inequality
+# Testing for inequality.
 car = 'porsche'
 house = 'small'
 print("Is car == house? I predict False.")
@@ -10,7 +10,7 @@ print(car == house)
 print("\nIs car == 'porsche'? I predict True.")
 print(car == 'porsche')
 
-# Testing with lower()
+# Testing with lower().
 country = 'Japan'
 print("\nIs country == 'Japan'? I predict False.")
 print(country.lower() == 'Japan')
@@ -18,7 +18,7 @@ print(country.lower() == 'Japan')
 print("\nIs country == 'japan'? I predict True.")
 print(country.lower() == 'japan')
 
-# Numerical tests
+# Numerical tests.
 age_1 = 18
 age_2 = 23
 print("\nIs 18 == 23? I predict False.")
@@ -39,7 +39,7 @@ print(age_1 >= age_2)
 print("\nIs 18 <= 23? I predict True.")
 print(age_1 <= age_2)
 
-# Test with 'and' and 'or'
+# Test with 'and' and 'or'.
 age_1 = 18
 age_2 = 23
 print("\nIs 18 or 23 equal to or greater than 20? I predict True.")
@@ -48,15 +48,23 @@ print(age_1 >= 20 or age_2 >= 20)
 print("\nIs 18 and 23 equal to or greater than 20? I predict False.")
 print(age_1 >= 20 and age_2 >= 20)
 
-# Testing if an item is in a list
+# Testing if an item is in a list.
 certifications = ['az-400', 'az-900', 'az-104']
 if 'az-400' or 'az-104' in certifications:
     print("\nYou're qualified!")
 else:
     print("\nYou are not qualified yet.")
 
-# Testing if an item is not in the list
+# Testing if an item is not in the list.
 if 'MS-500' not in certifications:
     print("\nYou are missing certification: 'MS-500'.")
 else:
     print("\nAll certifications acquired!")
+
+# Testing conditions without if statements.
+subjects = ['math', 'science', 'physical education']
+print('\nIs math in the list of subjects? I predict True.')
+print('math' in subjects)
+
+print('\nIs society in the list of subjects? I predict False.')
+print('society' in subjects)
