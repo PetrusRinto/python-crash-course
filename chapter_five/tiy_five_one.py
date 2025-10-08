537728c96488ab0d11411d630d0b2032b63f87b3
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-08 06:55:31 +0200

    conditional tests

---
+++ chapter_five/tiy_five_one.py
@@ -0,0 +1,9 @@
+# Patrick 08.10.2025.
+# 5-1 Conditional Tests.
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
+
+print("\nIs car == 'audi'? I predict False.")
+print(car == 'audi')
