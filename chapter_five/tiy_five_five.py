a85a886accc9ba69554fec1ccf945fd6cf5c956b
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-10 07:12:21 +0200

    green, yellow, red! Now you can shoot them all.

---
+++ chapter_five/tiy_five_five.py
@@ -0,0 +1,31 @@
+# Patrick 10.10.2025.
+# 5-5 Alien Colors #3.
+
+alien_color = ('green', 'yellow', 'red')
+
+# 'if' is True.
+alien = alien_color[0]
+if 'green' in alien:
+    print(f"\n5 points for shooting a {alien} that alien!\n")
+elif 'yellow' in alien:
+    print(f"\n10 points for shooting a {alien} alien!\n")
+else:
+    print(f"\n15 points for shooting a {alien_color} alien!\n")
+
+# 'elif' is True.
+alien = alien_color[1]
+if 'green' in alien:
+    print(f"\n5 points for shooting a {alien} that alien!\n")
+elif 'yellow' in alien:
+    print(f"\n10 points for shooting a {alien} alien!\n")
+else:
+    print(f"\n15 points for shooting[1] a {alien} alien!\n")
+
+# 'else' is True.
+alien = alien_color[2]
+if 'green' in alien:
+    print(f"\n5 points for shooting a {alien} that alien!\n")
+elif 'yellow' in alien:
+    print(f"\n10 points for shooting a {alien} alien!\n")
+else:
+    print(f"\n15 points for shooting a {alien} alien!\n")
