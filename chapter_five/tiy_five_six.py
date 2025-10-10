5ac08143538376eaa991fbdbe48692cf4b3078c2
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-10 07:38:58 +0200

    5-6 Stages of Life. Complete.

---
+++ chapter_five/tiy_five_six.py
@@ -0,0 +1,17 @@
+# Patrick 10.10.2025.
+# 5-6 Stages of Life.
+
+age = 23
+
+if age < 2:
+    print('You are a baby!')
+elif age < 4:
+    print('You are a toddler!')
+elif age < 13:
+    print("You're a kid!")
+elif age < 20:
+    print("You're a teenager!")
+elif age < 65:
+    print("You're an adult!")
+else:
+    print("You are an elder!")
