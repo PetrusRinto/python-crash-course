c20d7f6715549e02182b9a1d77249e5a75c7ef77
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-13 06:23:36 +0200

    Printing favorite fruits

---
+++ chapter_five/tiy_five_seven.py
@@ -0,0 +1,21 @@
+# Patrick 13.10.2025.
+# 5-7 Favorite Fruit.
+
+# Fruit list.
+favorite_fruits = ['mango', 'kiwi', 'honey melon', 'pineapple']
+
+# Mango.
+if 'mango' in favorite_fruits:
+    print("Mangoes are tasty!")
+
+# Kiwi.
+if 'kiwi' in favorite_fruits:
+    print("\nYou really like kiwis!")
+
+# Honey melon.
+if 'honey melon' in favorite_fruits:
+    print("\nHoney melon is delicous!")
+
+# pineapple.
+if 'pineapple' in favorite_fruits:
+    print("\nPineapple is tasty and healthy!")
