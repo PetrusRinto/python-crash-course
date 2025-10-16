5ff5c1758eb8c6b721dbc90184ff999a562cbcb2
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-16 07:05:04 +0200

    Successful output on 5-10, but did not fulfill all conditions in the task.

--- chapter_five/tiy_five_ten.py
+++ chapter_five/tiy_five_ten.py
@@ -6,8 +6,9 @@ current_users = ['admin', 'hamilton', 'guido', 'barry', 'lovelace']
 new_users = ['turing', 'torvalds', 'guido', 'hopper', 'hamilton']
 
 # Comparing users with a loop
-for new_user in new_users:
-    if new_user in current_users:
-        print(f"Username '{new_user}' is taken, enter a new one!")
-    else:
-        print(f"Username '{new_user}' is available!")
+for current_user in current_users:
+    for new_user in new_users:
+        if new_user.lower() in current_user.lower():
+            print(f"Username '{new_user.title()}' is taken!")
+        else:
+            print(f"Username '{new_user.title()}' is available!")
