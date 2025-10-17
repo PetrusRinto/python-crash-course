e8aec507d56455ac8cb76ac8eacd9ee9e49efe6d
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-17 07:22:40 +0200

    6-1. Person. My first dictionary!

---
+++ chapter_six/tiy_six_one.py
@@ -0,0 +1,14 @@
+# Patrick 17.10.2025.
+# 6-1 Person.
+
+# Dictionary
+person = {
+    'first_name': 'margaret',
+    'last_name': 'hamilton',
+    'age': '89',
+    'city': 'paoli'
+    }
+
+print(f'\nName: {person['first_name'].title()} {person['last_name'].title()}.')
+print(f'Age: {person['age']}.')
+print(f'Born: {person['city'].title()}.\n')
