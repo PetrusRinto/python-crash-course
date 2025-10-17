e24438e514971636d946ddc6f3416b57fe5f2055
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-17 07:41:21 +0200

    6-2 Favorite numbers.

---
+++ chapter_six/tiy_six_two.py
@@ -0,0 +1,17 @@
+# Patrick 17.10.2025.
+# 6-2 Favorite Numbers.
+
+# Dictonary
+favorite_numbers = {
+    'bjørn': '2',
+    'natalie': '87',
+    'patrick': '6',
+    'oliver': '67',
+    'nicolay': '11',
+    }
+
+print(f"\nBjørn's favorite number: {favorite_numbers['bjørn']}")
+print(f"\nNatalie's favorite number: {favorite_numbers['natalie']}")
+print(f"\nPatrick's favorite number: {favorite_numbers['patrick']}")
+print(f"\nOliver's favorite number: {favorite_numbers['oliver']}")
+print(f"\nNicolay's favorite number: {favorite_numbers['nicolay']}\n")
