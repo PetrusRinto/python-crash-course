54e9f8c92747dfcf7ceae5c4fec266f0b47b4e4d
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-17 16:12:35 +0200

    6-3 Glossary

---
+++ chapter_six/tiy_six_three.py
@@ -0,0 +1,16 @@
+# Patrick 17.10.2025.
+# 6-3 Glossary.
+
+glossary = {
+    'slicing': 'A process of taking out portions of a list, tuple or string.',
+    'tuple': 'A unmodifiable list.',
+    'list': 'Stores string values or integers seperated with commas.',
+    'built-in function': 'A code that is made to perform one specific task.',
+    'float': 'A definition for numbers with decimals.',
+    }
+
+print(f"\nSlicing:\n {glossary['slicing']}\n")
+print(f"Tuple:\n {glossary['tuple']}\n")
+print(f"List:\n {glossary['list']}\n")
+print(f"Built-in function:\n {glossary['built-in function']}\n")
+print(f"Float:\n {glossary['float']}\n")
