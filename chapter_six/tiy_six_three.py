4938c14c20841502e0daed58bcc57d3929bd33fc
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-21 07:56:00 +0200

    6-4 Glossary 2. Looped and added more glossaries.

--- chapter_six/tiy_six_three.py
+++ chapter_six/tiy_six_three.py
@@ -1,16 +1,20 @@
 # Patrick 17.10.2025.
-# 6-3 Glossary.
+# 6-3 Glossary / 6-4 Glossary 2.
 
-glossary = {
+glossaries = {
     'slicing': 'A process of taking out portions of a list, tuple or string.',
-    'tuple': 'A unmodifiable list.',
-    'list': 'Stores string values or integers seperated with commas.',
-    'built-in function': 'A code that is made to perform one specific task.',
-    'float': 'A definition for numbers with decimals.',
+    'tuple': 'An unmodifiable list.',
+    'list': 'Stores data seperated with commas, inside brackets.',
+    'built-in function': 'Performs one dedicated task.',
+    'float': 'Numbers with decimals.',
+    'sorting': 'Sorting in alphabetical order and more. Permanent or not.',
+    'variable': 'Something defined with a name and contains data.',
+    'range': 'Range has 3 main values: start, end and step.',
+    'comment': 'Comments are used to explain code or make it more readable.',
+    'for loop': '"loops" for each item in lists, tuples and more.',
     }
 
-print(f"\nSlicing:\n {glossary['slicing']}\n")
-print(f"Tuple:\n {glossary['tuple']}\n")
-print(f"List:\n {glossary['list']}\n")
-print(f"Built-in function:\n {glossary['built-in function']}\n")
-print(f"Float:\n {glossary['float']}\n")
+# Looping through the dictionary.
+for glossary, meaning in glossaries.items():
+    print(f"\n{glossary.title()}:")
+    print(f"{meaning}")
