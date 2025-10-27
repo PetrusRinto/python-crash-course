0a84037787f4c483b083f41ea3267084a0702ec5
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-27 07:07:54 +0100

    Nesting a list in another dictionary. Favorite languages now have multiple languages per person.

---
+++ chapter_six/favorite_languages.py
@@ -0,0 +1,15 @@
+# Patrick 27.10.2025.
+# Nesting a list in a dictionary of favorite languages.
+
+favorite_languages = {
+    'jen': ['python', 'rust'],
+    'sarah': ['c'],
+    'edward': ['rust', 'go'],
+    'phil': ['python', 'haskell'],
+    }
+
+# Show each person's favorite language.
+for name, languages in favorite_languages.items():
+    print(f"\n{name.title()}'s favorite languages are:")
+    for language in languages:
+        print(f"\t{language.title()}")
