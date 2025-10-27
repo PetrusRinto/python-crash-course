53c32f662da464b9735ae9e05dc7b9b87d685db7
Patrick Lintho <pettersenlintho@gmail.com>
2025-10-27 07:16:11 +0100

    Improved favorite languages to have a different output for more than one, only one or no value at all in the nested list.

--- chapter_six/favorite_languages.py
+++ chapter_six/favorite_languages.py
@@ -6,10 +6,16 @@ favorite_languages = {
     'sarah': ['c'],
     'edward': ['rust', 'go'],
     'phil': ['python', 'haskell'],
+    'dagny': [],
     }
 
 # Show each person's favorite language.
 for name, languages in favorite_languages.items():
-    print(f"\n{name.title()}'s favorite languages are:")
+    if len(languages) > 1:
+        print(f"\n{name.title()}'s favorite languages are:")
+    elif len(languages) < 1:
+        print(f"\n{name.title()} have not taken the poll.")
+    elif len(languages) == 1:
+        print(f"\n{name.title()}'s favorite language is:")
     for language in languages:
         print(f"\t{language.title()}")
