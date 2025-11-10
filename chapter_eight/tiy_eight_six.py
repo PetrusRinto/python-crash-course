7274524baea58e97f7229965c7b3f03ab5eb3009
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-10 16:32:57 +0100

    8-6 City Names. Done!

---
+++ chapter_eight/tiy_eight_six.py
@@ -0,0 +1,11 @@
+# Patrick 10.11.2025.
+# 8-6 City Names.
+
+def city_country(city, country):
+    """Takes in a city and a country value."""
+    city_country = f"\n{city.title()}, {country.title()}"
+    print(city_country)
+
+city_country('hokkaido', 'japan')
+city_country('beijing', 'china')
+city_country('verdal', 'norway')
