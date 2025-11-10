c56763c6d2ef4b1af8104bbb4bf648b828d18ad3
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-10 11:48:19 +0100

    8-2 Favorite Book. Done!

---
+++ chapter_eight/tiy_eight_two.py
@@ -0,0 +1,7 @@
+# Patrick 10.11.2025.
+# 8-2 Favoreite Book.
+
+def favorite_book(title):
+    """Displays message with a book-title"""
+    print(f"One of my favorite books is {title}.")
+favorite_book('Python Crash Course')
