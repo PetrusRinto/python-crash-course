a4e592db6e476ad1af0c83a2e60cf508902c8d5b
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-10 11:44:54 +0100

    8-1 Message. Done!

---
+++ chapter_eight/tiy_eight_one.py
@@ -0,0 +1,7 @@
+# Patrick 10.11.2025.
+# 8-1 Message.
+
+def display_message():
+    """Displaying what I am learning"""
+    print("In this chapter I am learning about functions!")
+display_message()
