cbdb4cf7fa043f91a26a0e61a00503bbae6655d9
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-11 22:03:47 +0100

    8-14 Cars. Done!

---
+++ chapter_eight/tiy_eight_fourteen.py
@@ -0,0 +1,13 @@
+# Patrick 11.11.2025.
+# 8-14 Cars.
+
+def make_car(manufacturer, model, **car_info):
+    """Build a dictionary containing information about a car"""
+    car_info['manufacturer'] = manufacturer
+    car_info['model'] = model
+    return car_info
+
+car = make_car('toyota', 'aygo',
+                color='green',
+                facelift=True)
+print(car)
