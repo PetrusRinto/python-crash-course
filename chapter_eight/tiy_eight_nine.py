cbe3a85d08dc271f2169beb8ac238cfc081730a3
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-11 12:18:42 +0100

    8-9 Messages. Done!

---
+++ chapter_eight/tiy_eight_nine.py
@@ -0,0 +1,15 @@
+# Patrick 11.11.2025.
+# 8-9 Messages.
+
+def show_messages(messages):
+    """Prints a simple message from a list."""
+    for message in messages:
+        print(f"\n{message}")
+
+message_list = [
+    "This is a text.",
+    "Haha, that's funny!",
+    "Is there bedrock before China?"
+    ]
+
+show_messages(message_list)
