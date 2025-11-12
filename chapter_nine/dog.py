a1867d4175921c23c59f7782d408add35926f2a3
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-12 21:25:11 +0100

    Familiarizing with classes.

---
+++ chapter_nine/dog.py
@@ -0,0 +1,19 @@
+# Patrick 12.11.2025.
+# Get familiar with classes.
+# Dogs.
+
+class Dog:
+    """A simple attemt to model a dog."""
+
+    def __init__(self, name, age):
+        """Initialize name and age attributes."""
+        self.name = name
+        self.age = age
+    
+    def sit(self):
+        """Simulate a dog sitting in response to a command."""
+        print(F"{self.name} is now sitting.")
+    
+    def roll_over(self):
+        """Simulate rolling over in response to a command."""
+        print(f"{self.name} rolled over!")
