f9d5d57d90a30250bf653777851bb56106d3f2a3
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-12 07:39:03 +0100

    8-16 Imports. "a" done!

---
+++ chapter_eight/tiy_eight_sixteen.py
@@ -0,0 +1,9 @@
+# Patrick 12.11.2025.
+# 8-16 Imports.
+
+import tiy_eight_fourteen
+
+car = tiy_eight_fourteen.make_car('toyota', 'aygo',
+                                  color='green',
+                                  facelift=True)
+print(car)
