2eee6fbd5d252ea7ec16fa0ee2c22bbf362dc986
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-12 12:17:48 +0100

    8-16 Imports. "c" done!

--- chapter_eight/tiy_eight_sixteen.py
+++ chapter_eight/tiy_eight_sixteen.py
@@ -1,9 +1,7 @@
 # Patrick 12.11.2025.
 # 8-16 Imports.
 
-from tiy_eight_fourteen import make_car
+from tiy_eight_fourteen import make_car as mc
 
-car = make_car('toyota', 'aygo',
-                color='green',
-                facelift=True)
+car = mc('toyota', 'aygo', color='green', facelift=True)
 print(car)
