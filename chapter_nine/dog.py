ab59f2d8a36f78822e28c9adadd0e1f0c15d131b
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-13 06:45:38 +0100

    Added "your" dog.

--- chapter_nine/dog.py
+++ chapter_nine/dog.py
@@ -19,3 +19,12 @@ class Dog:
         print(f"{self.name} rolled over!")
 
 my_dog = Dog('Willie', 6)
+your_dog = Dog('Lucy', 3)
+
+print(f"My dog's name is {my_dog.name}.")
+print(f"My dog is {my_dog.age} years old.")
+my_dog.sit()
+
+print(f"Your dog's name is {my_dog.name}.")
+print(f"Your dog is {my_dog.age} years old.")
+your_dog.sit()
