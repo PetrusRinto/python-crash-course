b093d48e4435d11dff6af796615b7798e750af32
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-14 07:20:12 +0100

    Created car class from python crash course book and follow along to make it stick better (in my head).

--- chapter_nine/tiy_nine_three.py
+++ chapter_nine/tiy_nine_three.py
@@ -6,7 +6,6 @@ class User:
 
     def __init__(self, first_name, last_name, title, role):
         """Initializing attributes for the user profile."""
-
         self.first_name = first_name
         self.last_name = last_name
         self.title = title
@@ -14,7 +13,6 @@ class User:
     
     def describe_user(self):
         """A method that summurize the user's details."""
-
         print(f"\n'{self.first_name}' user info:")
         print(f"\tFull name: {self.first_name} {self.last_name}")
         print(f"\tTitle: {self.title}")
@@ -22,7 +20,6 @@ class User:
     
     def greet_user(self):
         """A method for greeting the user."""
-
         print(f"Greetings {self.first_name}!\n")
 
 user_1 = User('Petrus', 'Rinto', 'Cloud Engineer', 'Global Reader')
