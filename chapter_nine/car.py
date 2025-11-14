cc7febe2144c66139b930dca02bf763580a45994
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-14 07:39:05 +0100

    Added a method for incrementing the mileage.

--- chapter_nine/car.py
+++ chapter_nine/car.py
@@ -30,8 +30,15 @@ class Car:
         else:
             print("You can't roll back an odometer!")
     
-my_new_car = Car('audi', 'a4', '2024')
-print(my_new_car.get_descriptive_name())
+    def increment_odometer(self, miles):
+        """Add the given amount to the odometer reading."""
+        self.odometer_reading += miles
+    
+my_used_car = Car('toyota', 'aygo', '2011')
+print(my_used_car.get_descriptive_name())
 
-my_new_car.update_odometer(23)
-my_new_car.read_odometer()
+my_used_car.update_odometer(23_500)
+my_used_car.read_odometer()
+
+my_used_car.increment_odometer(100)
+my_used_car.read_odometer()
