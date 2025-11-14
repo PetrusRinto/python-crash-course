2b517ef212195b35a1458002440eb09eaf9573b7
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-14 07:33:19 +0100

    Logic that rejects rolling the odometer backwards.

--- chapter_nine/car.py
+++ chapter_nine/car.py
@@ -21,8 +21,14 @@ class Car:
         print(f"This car has {self.odometer_reading} miles on it.")
     
     def update_odometer(self, mileage):
-        """Set the odometer reading to the given value."""
-        self.odometer_reading = mileage
+        """
+        Set the odometer reading to the given value.
+        Reject the change if it attempts to roll the odometer back.
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
     
 my_new_car = Car('audi', 'a4', '2024')
 print(my_new_car.get_descriptive_name())
