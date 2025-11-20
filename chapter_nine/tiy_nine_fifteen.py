3c3c8804d63215dad4be87155eb1448bbde60016
Patrick Lintho <pettersenlintho@gmail.com>
2025-11-20 07:07:53 +0100

    9-15 Lottery Analysis. Done!

--- chapter_nine/tiy_nine_fifteen.py
+++ chapter_nine/tiy_nine_fifteen.py
@@ -1,4 +1,4 @@
-# Patrick 19.11.2025.
+# Patrick 19.11.2025. / 20.11.2025.
 # 9-15 Lottery Analysis.
 
 from random import choice
@@ -21,8 +21,20 @@ class Lottery:
             pick = choice(available)
             ticket.append(pick)
             available.remove(pick)
-        exit
-        print(ticket)
+        return ticket
 
 lottery = Lottery()
 lottery.pull_items()
+
+my_ticket = [2, 'e', 'c', 6]
+
+attempts = 0
+won = False
+
+while not won:
+    attempts += 1
+    pull = lottery.pull_items()
+
+    if pull == my_ticket:
+        print(f"It took {attempts} attempts for you to win!")
+        won = True
