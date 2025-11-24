908db362d13e5b5695f37be50d9ba9a559970aa2
PetrusRinto <pettersenlintho@gmail.com>
2025-11-24 16:53:53 +0100

    10-3 Simpler Code. Done!

--- chapter_ten/tiy_ten_one.py
+++ chapter_ten/tiy_ten_one.py
@@ -7,6 +7,5 @@ path = Path('learning_python.txt')
 contents = path.read_text()
 print(contents)
 
-lp_list = contents.splitlines()
-for line in lp_list:
+for line in contents.splitlines():
     print(line)
