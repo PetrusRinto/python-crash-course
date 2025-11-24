3beee1373d3ac3c574674416d6a9992ae15b1050
PetrusRinto <pettersenlintho@gmail.com>
2025-11-24 12:23:45 +0100

    Accessing the file's lines with splitlines()

--- chapter_ten/file_reader.py
+++ chapter_ten/file_reader.py
@@ -4,5 +4,8 @@
 from pathlib import Path
 
 path = Path('pi_digits.txt')
-contents = path.read_text().rstrip()
-print(contents)
+contents = path.read_text()
+
+lines = contents.splitlines()
+for line in lines:
+    print(line)
