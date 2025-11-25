7e4892c2686c9972aebae8314315b43ee1b436dc
PetrusRinto <pettersenlintho@gmail.com>
2025-11-25 16:21:50 +0100

    Counting the amount of words in alice.txt.

--- chapter_ten/alice.py
+++ chapter_ten/alice.py
@@ -9,3 +9,8 @@ try:
     contents = path.read_text(encoding='utf-8')
 except FileNotFoundError:
     print(f"Sorry, the file {path} does not exist.")
+else:
+    # Count the approximate number of words in the text file.
+    words = contents.split()
+    num_words = len(words)
+    print(f"The file {path} has about {num_words} words.")
