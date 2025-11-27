9959991b2e9048f3bb9f0a55f24eb84d864d9437
PetrusRinto <pettersenlintho@gmail.com>
2025-11-27 07:12:16 +0100

    10-9 Silent Cats and Dogs. Done!

--- chapter_ten/tiy_ten_nine.py
+++ chapter_ten/tiy_ten_nine.py
@@ -1,3 +1,16 @@
 # Patrick 27.11.2025.
 # 10-9 Silent Cats and Dogs.
 
+from pathlib import Path
+
+filenames = ['cats.txt', 'dogs.txt']
+
+for filename in filenames:
+    path = Path(filename)
+
+    try:
+        contents = path.read_text(encoding='utf-8')
+    except FileNotFoundError:
+        pass
+    else:
+        print(contents)
