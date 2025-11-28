246ff3ee6ec12606bc1588a91846e4df4190237d
PetrusRinto <pettersenlintho@gmail.com>
2025-11-28 07:06:38 +0100

    Refactored into three functions.

--- chapter_ten/practice/data_store/remember_me.py
+++ chapter_ten/practice/data_store/remember_me.py
@@ -12,6 +12,13 @@ def get_stored_username(path):
         return username
     else:
         return None
+    
+def get_new_username(path):
+    """Prompt for a new username."""
+    username = input('What is your name? ').lower()
+    contents = json.dumps(username)
+    path.write_text(contents)
+    return username
 
 def greet_user():
     """Greet the user by name."""
@@ -20,9 +27,7 @@ def greet_user():
     if username:
         print(f"Welcome back, {username.title()}!")
     else:
-        username = input('What is your name? ').lower()
-        contents = json.dumps(username)
-        path.write_text(contents)
+        username = get_new_username(path)
         print(f"We'll remember you when you come back, {username.title()}!")
 
 greet_user()
