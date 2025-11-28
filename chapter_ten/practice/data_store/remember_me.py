6f4c9ca4cd171158489ab9abb61f238dad5050d0
PetrusRinto <pettersenlintho@gmail.com>
2025-11-28 06:57:33 +0100

    Refoctored into a function.

--- chapter_ten/practice/data_store/remember_me.py
+++ chapter_ten/practice/data_store/remember_me.py
@@ -4,15 +4,17 @@
 from pathlib import Path
 import json
 
-path = Path('username.json')
+def greet_user():
+    """Greet the user by name."""
+    path = Path('username.json')
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        print(f"Welcome back, {username.title()}!")
+    else:
+        username = input('What is your name? ').lower()
+        contents = json.dumps(username)
+        path.write_text(contents)
+        print(f"We'll remember you when you come back, {username.title()}!")
 
-# New pathlib module instead of try-except-else.
-if path.exists():
-    contents = path.read_text()
-    username = json.loads(contents)
-    print(f"Welcome back, {username.title()}!")
-else:
-    username = input('What is your name? ').lower()
-    contents = json.dumps(username)
-    path.write_text(contents)
-    print(f"We'll remember you when you come back, {username.title()}!")
+greet_user()
