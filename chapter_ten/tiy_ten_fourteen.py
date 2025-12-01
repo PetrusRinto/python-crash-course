0478cc0b74bbc1d1a8569204d4101645a47babbf
PetrusRinto <pettersenlintho@gmail.com>
2025-12-01 12:33:27 +0100

    10-14 Verify User. Done!

---
+++ chapter_ten/tiy_ten_fourteen.py
@@ -0,0 +1,48 @@
+# Patrick 01.12.2025.
+# 10-14 Verify User.
+
+from pathlib import Path
+import json
+
+def get_stored_data(path):
+    """Get stored username if available."""
+    if path.exists():
+        contents = path.read_text()
+        data = json.loads(contents)
+        return data
+    else:
+        return None
+    
+def get_new_data(path):
+    """Prompt for new data."""
+    username = input('What is your name? ').lower()
+    profession = input('What is your profession? ').lower()
+    age = input('What is your age? ')
+
+    data = {
+        'username': f'{username}',
+        'profession': f'{profession}',
+        'age': f'{age}'
+        }
+
+    contents = json.dumps(data)
+    path.write_text(contents)
+    return data
+
+def greet_user():
+    """Greet the user by name."""
+    path = Path('data.json')
+    data = get_stored_data(path)
+    verify = input(f'Is {data['username'].title()} your name? (y/n) ')
+
+    if verify == 'y':
+        print(f"Welcome back, {data['username'].title()}!")
+        print('\nThis is our saved data about you:')
+        print(f'Username: {data['username'].title()}')
+        print(f'Profession: {data['profession'].title()}')
+        print(f'Age: {data['age']}')
+    else:
+        data = get_new_data(path)
+        print(f"We'll remember you when you come back, {data['username'].title()}!")
+
+greet_user()
