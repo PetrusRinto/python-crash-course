2704ba489cc93f5af3bab660bd05c739c768d514
PetrusRinto <pettersenlintho@gmail.com>
2025-12-05 06:28:27 +0100

    11-1 City, Country. Done! My first test!

---
+++ chapter_eleven/tiy_eleven_one/city_functions.py
@@ -0,0 +1,7 @@
+# Patrick 04.12.2025.
+# 11-1 City, Country.
+
+def get_city_country(city, country):
+    """A function that returns a city and a country."""
+    city_country = f"{city}, {country}" 
+    return city_country.title()
