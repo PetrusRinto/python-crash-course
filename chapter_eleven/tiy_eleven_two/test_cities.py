dbac0e270b54ade8bdea440905e4fb893b4ac70c
PetrusRinto <pettersenlintho@gmail.com>
2025-12-05 07:38:31 +0100

    11-2 Population. Part 3, extra test passed. Done!

--- chapter_eleven/tiy_eleven_two/test_cities.py
+++ chapter_eleven/tiy_eleven_two/test_cities.py
@@ -5,5 +5,10 @@ from city_functions import get_city_country
 
 def test_city_country():
     """Testing if 'Oslo, Norway' works."""
-    city_country = get_city_country('oslo', 'norway', '1100000')
+    city_country = get_city_country('oslo', 'norway')
+    assert city_country == 'Oslo, Norway'
+
+def test_city_country_population():
+    """Testing if 'Oslo, Norway - population xxx' works."""
+    city_country = get_city_country('oslo', 'norway', population=1100000)
     assert city_country == 'Oslo, Norway - population 1100000'
