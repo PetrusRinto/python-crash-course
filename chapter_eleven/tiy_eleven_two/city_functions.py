3faf19d3b08a8f9cf425f0723987dcf00629a063
PetrusRinto <pettersenlintho@gmail.com>
2025-12-05 07:10:41 +0100

    11-2 Population. Part 2, test pass. Done!

--- chapter_eleven/tiy_eleven_two/city_functions.py
+++ chapter_eleven/tiy_eleven_two/city_functions.py
@@ -1,7 +1,12 @@
 # Patrick 05.12.2025.
 # 11-2 Population.
 
-def get_city_country(city, country, population):
+def get_city_country(city, country, population=''):
     """A function that returns a city and a country."""
-    city_country = f"{city}, {country} - population {population}" 
-    return city_country.title()
+    city = city.lower().title()
+    country = country.lower().title()
+    if population:
+        city_country = f"{city}, {country} - population {population}"
+    else:
+        city_country = f"{city}, {country}"
+    return city_country
--- chapter_eleven/tiy_eleven_two/test_cities.py
+++ chapter_eleven/tiy_eleven_two/test_cities.py
@@ -5,5 +5,5 @@ from city_functions import get_city_country
 
 def test_city_country():
     """Testing if 'Oslo, Norway' works."""
-    city_country = get_city_country('oslo', 'norway')
-    assert city_country == 'Oslo, Norway'
+    city_country = get_city_country('oslo', 'norway', '1100000')
+    assert city_country == 'Oslo, Norway - population 1100000'
