099ee1e4aca6d61fe0eea311312b41ab6ee41eab
PetrusRinto <pettersenlintho@gmail.com>
2025-12-08 21:35:52 +0100

    11-3 Employee. With fixture, Done!

--- chapter_eleven/tiy_eleven_three/test_employee.py
+++ chapter_eleven/tiy_eleven_three/test_employee.py
@@ -2,15 +2,20 @@
 # 11-3 Employee test file.
 
 from employee import Employee
+import pytest
 
-def test_give_default_raise():
-    """Testing the default salary raise value."""
+@pytest.fixture
+def employee():
+    """Representation of employee's name and annual salary."""
     employee = Employee('roberta', 'bot', 100000)
+    return employee
+
+def test_give_default_raise(employee):
+    """Testing the default salary raise value."""
     default_raise = employee.give_raise()
     assert default_raise == 105000
 
-def test_give_custom_raise():
+def test_give_custom_raise(employee):
     """Testing the custom salary raise."""
-    employee = Employee('roberta', 'bot', 100000)
     custom_raise = employee.give_raise(100000)
     assert custom_raise == 200000
