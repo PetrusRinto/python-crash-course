6831d3412c86c69d2dd2895b9bf26c74a3fe8ec1
PetrusRinto <pettersenlintho@gmail.com>
2025-12-09 06:59:22 +0100

    Made an instance of Settings.

--- projects/alien_invasion/alien_invasion.py
+++ projects/alien_invasion/alien_invasion.py
@@ -5,6 +5,8 @@ import sys
 
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Oveall class to manage game assets and behavior."""
 
@@ -12,8 +14,10 @@ class AlienInvasion:
         """Initializing the game, and create game resources."""
         pygame.init()
         self.clock = pygame.time.Clock()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200,800))
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
 
         # Set the background color.
