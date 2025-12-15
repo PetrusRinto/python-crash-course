4a1ab47b3ddc29156fa4aaf9f9a54e1aefe93426
PetrusRinto <pettersenlintho@gmail.com>
2025-12-15 11:53:15 +0100

    12-4 Rocket. Sat the background color.

--- projects/chapter_twelve/rocket_man/tiy_twelve_four.py
+++ projects/chapter_twelve/rocket_man/tiy_twelve_four.py
@@ -16,6 +16,8 @@ class RocketMan:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Rocket Man")
 
+        self.bg_color = (0, 0, 0)
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
@@ -23,6 +25,9 @@ class RocketMan:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()
+            
+            # Redraw the screen during each pass through the loop.
+            self.screen.fill(self.bg_color)
                 
             # Make the most recently drawn screen visible.
             pygame.display.flip()
