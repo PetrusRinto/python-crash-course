e53edacd3b7be822d88a4110684d497981734d68
PetrusRinto <pettersenlintho@gmail.com>
2025-12-22 13:44:58 +0100

    12-4 Rocket. Refactoring key events.

--- projects/chapter_twelve/rocket_man/tiy_twelve_four.py
+++ projects/chapter_twelve/rocket_man/tiy_twelve_four.py
@@ -37,24 +37,31 @@ class RocketMan:
             if event.type == pygame.QUIT:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_RIGHT:
-                    self.rocket.moving_right = True
-                elif event.key == pygame.K_LEFT:
-                    self.rocket.moving_left = True
-                elif event.key == pygame.K_UP:
-                    self.rocket.moving_up = True
-                elif event.key == pygame.K_DOWN:
-                    self.rocket.moving_down = True
-
+                self._check_keydown_events(event)
             elif event.type == pygame.KEYUP:
-                if event.key == pygame.K_RIGHT:
-                    self.rocket.moving_right = False
-                elif event.key == pygame.K_LEFT:
-                    self.rocket.moving_left = False
-                elif event.key == pygame.K_UP:
-                    self.rocket.moving_up = False
-                elif event.key == pygame.K_DOWN:
-                    self.rocket.moving_down = False
+                self._check_keyup_events(event)
+
+    def _check_keydown_events(self, event):
+        """Respond to keypresses."""
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = True
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = True
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = True
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = True
+    
+    def _check_keyup_events(self, event):
+        """Respond to key releases."""
+        if event.key == pygame.K_RIGHT:
+            self.rocket.moving_right = False
+        elif event.key == pygame.K_LEFT:
+            self.rocket.moving_left = False
+        elif event.key == pygame.K_UP:
+            self.rocket.moving_up = False
+        elif event.key == pygame.K_DOWN:
+            self.rocket.moving_down = False
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
