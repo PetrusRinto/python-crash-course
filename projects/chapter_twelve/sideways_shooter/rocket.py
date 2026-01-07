0bc86c5c9ba3b54ce058c4a4679359978ea5bc34
PetrusRinto <pettersenlintho@gmail.com>
2026-01-07 16:09:56 +0100

    12-6 Sideways Shooter. Created class to manage the rocket.

--- projects/chapter_twelve/sideways_shooter/rocket.py
+++ projects/chapter_twelve/sideways_shooter/rocket.py
@@ -1,3 +1,40 @@
 # Patrick 06.01.2026.
 # 12-6 Sideways Shooter.
 
+import pygame
+
+class Rocket:
+    """A class to manage the rocket."""
+
+    def __init__(self, ss_game):
+        """Initialize the rocket and set its starting position."""
+        self.screen = ss_game.screen
+        self.settings = ss_game.settings
+        self.screen_rect = ss_game.screen.get_rect()
+
+        self.image = pygame.image.load('images/rocket.bmp')
+        self.rect = self.image.get_rect()
+
+        self.rect.left = self.screen_rect.left
+
+        self.x = float(self.rect.x)
+        self.y = float(self.rect.y)
+
+        # Movement flag.
+        self.moving_up = False
+        self.moving_down = False
+
+    def update(self):
+        """Update the rocket's position based on the movement flag."""
+        if self.moving_up and self.rect.top > 0:
+            self.y -= self.settings.rocket_speed
+        if self.moving_down and self.rect.bottom < self.screen_rect.bottom:
+            self.y += self.settings.rocket_speed
+        
+        # Update rect object from self.x and self.y.
+        self.rect.x = self.x
+        self.rect.y = self.y
+    
+    def blitme(self):
+        """Draw the rocket at its current position."""
+        self.screen.blit(self.image, self.rect)
