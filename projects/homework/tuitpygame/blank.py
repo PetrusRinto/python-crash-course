ef9b79a4bfc5af48699d22064974d5eccbbf39af
PetrusRinto <pettersenlintho@gmail.com>
2026-01-15 14:56:21 +0100

    Exercise

--- projects/homework/tuitpygame/blank.py
+++ projects/homework/tuitpygame/blank.py
@@ -10,12 +10,17 @@ pygame.init()
 screen = pygame.display.set_mode((600, 400))
 pygame.display.set_caption("Homework")
 clock = pygame.time.Clock()
+test_surface = pygame.Surface((100, 200))
+test_surface.fill('red')
+
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+    
+    screen.blit(test_surface,(200, 100))
 
     pygame.display.update()
     clock.tick(60)
