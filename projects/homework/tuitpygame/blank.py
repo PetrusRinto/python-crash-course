442a0e83252dea2de11b93830c0e941bb5f05d25
PetrusRinto <pettersenlintho@gmail.com>
2026-01-15 13:03:04 +0100

    Naming and frame rate.

--- projects/homework/tuitpygame/blank.py
+++ projects/homework/tuitpygame/blank.py
@@ -1,15 +1,21 @@
 # Patrick 15.01.2026.
 # Homework...
 
+import sys
+
 import pygame
 
 pygame.init()
 
 screen = pygame.display.set_mode((600, 400))
+pygame.display.set_caption("Homework")
+clock = pygame.time.Clock()
 
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            sys.exit()
 
     pygame.display.update()
+    clock.tick(60)
