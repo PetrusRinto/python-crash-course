835328fedf50b2b7b915da94343ed775ee1e90da
PetrusRinto <pettersenlintho@gmail.com>
2026-01-20 12:25:42 +0100

    Added moving snail and updated it's position.

--- projects/homework/tuitpygame/blank.py
+++ projects/homework/tuitpygame/blank.py
@@ -16,6 +16,9 @@ sky_surface = pygame.image.load('graphics/sky.png')
 ground_surface = pygame.image.load('graphics/ground.png')
 text_surface = test_font.render('My game', False, 'black')
 
+snail_surface = pygame.image.load('graphics/snail/snail1.png')
+snail_x_pos = 600
+
 
 while True:
     for event in pygame.event.get():
@@ -26,6 +29,10 @@ while True:
     screen.blit(sky_surface,(0, 0))
     screen.blit(ground_surface,(0, 300))
     screen.blit(text_surface,(300, 50))
+    snail_x_pos -= 4
+    if snail_x_pos <= -100:
+        snail_x_pos = 800
+    screen.blit(snail_surface,(snail_x_pos, 265))
 
     pygame.display.update()
     clock.tick(60)
