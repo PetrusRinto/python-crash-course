270d432603569479ee90c4996207f9f13e604b17
PetrusRinto <pettersenlintho@gmail.com>
2026-01-27 15:23:32 +0100

    Based snail movement to the x-axis.

--- projects/homework/tuitpygame/blank.py
+++ projects/homework/tuitpygame/blank.py
@@ -16,8 +16,8 @@ sky_surface = pygame.image.load('graphics/sky.png').convert()
 ground_surface = pygame.image.load('graphics/ground.png').convert()
 text_surface = test_font.render('My game', False, 'black')
 
-snail_surface = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
-snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+snail_surf = pygame.image.load('graphics/snail/snail1.png').convert_alpha()
+snail_rect = snail_surf.get_rect(midbottom = (600, 300))
 
 player_surf = pygame.image.load('graphics/player/player_walk_1.png').convert_alpha()
 player_rect = player_surf.get_rect(midbottom = (80, 300))
@@ -31,10 +31,10 @@ while True:
     screen.blit(sky_surface,(0, 0))
     screen.blit(ground_surface,(0, 300))
     screen.blit(text_surface,(300, 50))
-    snail_rect.right -= 4
+    snail_rect.x -= 4
     if snail_rect.right < 0:
         snail_rect.left = 800
-    screen.blit(snail_surface, snail_rect)
+    screen.blit(snail_surf, snail_rect)
     screen.blit(player_surf, player_rect)
 
     pygame.display.update()
