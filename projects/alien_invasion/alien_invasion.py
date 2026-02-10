053f0873e4f5909f8a158ee96812efec2a7680f2
PetrusRinto <pettersenlintho@gmail.com>
2026-02-10 13:47:08 +0100

    Created a update method for aliens.

--- projects/alien_invasion/alien_invasion.py
+++ projects/alien_invasion/alien_invasion.py
@@ -35,6 +35,7 @@ class AlienInvasion:
             self._check_events()
             self.ship.update()
             self._update_bullets()
+            self._update_aliens()
             self._update_screen()
             self.clock.tick(60)
     
@@ -106,6 +107,10 @@ class AlienInvasion:
         for bullet in self.bullets.copy():
             if bullet.rect.bottom <= 0:
                 self.bullets.remove(bullet)
+    
+    def _update_aliens(self):
+        """Update the positions of all aliens in the fleet."""
+        self.aliens.update()
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
