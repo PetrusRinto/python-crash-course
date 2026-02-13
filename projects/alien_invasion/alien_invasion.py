adbad6f7de815493dbc2031472dc0035bbc16188
PetrusRinto <pettersenlintho@gmail.com>
2026-02-13 15:57:06 +0100

    Alien Invasion. Added check for alien ship collisions.

--- projects/alien_invasion/alien_invasion.py
+++ projects/alien_invasion/alien_invasion.py
@@ -139,6 +139,10 @@ class AlienInvasion:
         """Update the positions of all aliens in the fleet."""
         self._check_fleet_edges()
         self.aliens.update()
+
+        # Look for alien-ship collisions.
+        if pygame.sprite.spritecollideany(self.ship, self.aliens):
+            print("Ship hit!!!")
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
