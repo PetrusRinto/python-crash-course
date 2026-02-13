64eff14c5c74a3243de3fe09fa0aa84034b4b3b3
PetrusRinto <pettersenlintho@gmail.com>
2026-02-13 11:32:23 +0100

    Made bullet wider.

--- projects/chapter_twelve/sideways_shooter/settings.py
+++ projects/chapter_twelve/sideways_shooter/settings.py
@@ -17,7 +17,7 @@ class Settings:
         # Bullet settings.
         self.bullet_speed = 10.0
         self.bullet_width = 15
-        self.bullet_height = 3
+        self.bullet_height = 6
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
 
